118d04df9de1ce4adab1bc543e385c0294686b8b
Ivar Johansson <ivar@webbmaffian.se>
2023-01-31 18:38:17 +0100

    Allow TLDs of more than 4 characters

--- validators/email.go
+++ validators/email.go
@@ -11,7 +11,7 @@ import (
 var emailRegex *regexp.Regexp
 
 func init() {
-	emailRegex = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	emailRegex = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`)
 }
 
 func Email(value, parent reflect.Value, arg string, opt *utils.Options) (err error) {
